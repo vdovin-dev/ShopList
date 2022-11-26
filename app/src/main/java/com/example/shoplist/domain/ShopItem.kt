6841e9d98a967279c74448bf21226c3c728368a3
VALERIY <117378616+vdovin-dev@users.noreply.github.com>
2022-11-26 16:00:12 +0300

    Comments added

--- app/src/main/java/com/example/shoplist/domain/ShopItem.kt
+++ app/src/main/java/com/example/shoplist/domain/ShopItem.kt
@@ -1,9 +1,14 @@
 package com.example.shoplist.domain
 
 data class ShopItem(
-    val id: Int,
+
     val name:String,
     val count: Int,
     val enabled:Boolean,
+    var id: Int = UNDEFIEND_ID,
 
-)
+){
+    companion object{
+        const val UNDEFIEND_ID = -1
+    }
+}
